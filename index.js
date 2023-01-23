5e41439614bae67bfe20a2dec8db2e4e6b7b88bc
aerinf <113934364+aerinf@users.noreply.github.com>
2023-01-22 21:21:44 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const genres = [
+//assign array of music genres with ids
+const genres = 
+[
     {id: 1, name: "pop"},
     {id: 2, name: "hiphop"},
     {id: 3, name: "rap"},
@@ -38,34 +40,33 @@ app.get('/api/genres/:id', (req,res)=>{
     res.status(200).send(genre);
 })
 //genres by date
-app.get('/api/genres/year/month', (req,res)=>{
+app.get('/api/genres/:year/:month', (req,res)=>{
     const genre = genres.filter(genre => genre.year=== parseInt(req.params.year) && genre.month=== parseInte(req.params.month));
-    if (!genre) {
-        res.status(404).send("No results found for " + req.params.year + " " + req.params.month);
+    if (genre.length===0) {
+        res.status(404).send("No results found for " + req.params.month + "/" + req.params.year);
         return
     }
-    res.status(200).send(genre);
+    else {
+        res.status(200).send(genre);
+    }
 });
 
 
 //HTTP POST REQUESTS
 app.post('/api/genres', (req,res) => {
         const genre ={
-            //we assign an ID and a name property
+            //assign an ID and a name property
             id: genres.length +1,
             name:req.body.name
         }
-        if (genre.name.length>=3 && genre.name<=15) {
-
-        //next step: push it to the array
-        courses.push(genre);
+        if (genre["name"].length>=3 && genre["name"].length<=15) {
+        //push it to the array
+        genres.push(genre);
         res.status(200).send(genre);
-        //next step: the server should return the new resource to the client in the body of the response 
         }
         else {
             res.status(400).send("The genre name should be between 3 and 15 characters.");
-        }
-        
+        } 
 });
 
 
@@ -78,7 +79,7 @@ app.put('/api/genres/:id', (req,res)=>{
         return
     }
     else {
-        if (genre.name.length>=3 && genre.name<=15) {
+        if (req.body.name.length>=3 && req.body.name.length<=15) {
             genre["name"] = req.body.name;
             res.status(200).send(genre);
         }
@@ -97,11 +98,12 @@ app.delete('/api/genres/:id', (req,res)=>{
         return
     }
     else {
-        genres.splice(genres.indexOf(genre));
+        genres.splice(genres.indexOf(genre), 1);
         res.status(200).send("Genre was deleted.");
     }
 });
 
+//RUN SERVER
 app.listen(3000, ()=> {
     console.log('Listening on port 3000 ...')
 })
